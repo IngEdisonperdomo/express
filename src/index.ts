f07527a05028c9c7855f4eebd221cf6056c23f7a
leonardo <leonardo.alvarado@tektonlabs.com>
2023-01-25 18:01:26 -0400

    init express

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import diariesRouter from "./routes/diaries";
+
+const app = express();
+app.use(express.json()); // for parsing application/json
+
+const PORT = 3000;
+
+app.get("/ping", (_req, res) => {
+  console.log("ping");
+  res.send("pong");
+});
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
+
+app.use("/api/diaries", diariesRouter);
